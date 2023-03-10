fdc8b56290c28cd0a9e92fd927ab6341cbecd60e
Romzes <roman.usachev@gmail.com>
2023-03-10 18:14:34 +0300

    BinaryTree

--- MainProject/Explore/Learn/10_BinaryTree/2_Medium/00105.py
+++ MainProject/Explore/Learn/10_BinaryTree/2_Medium/00105.py
@@ -1,34 +1,34 @@
-# 106. Construct Binary Tree from Inorder and Postorder Traversal
-# Given two integer arrays inorder and postorder
-# where inorder is the inorder traversal of a binary tree
-# and postorder is the postorder traversal of the same tree,
+# 105. Construct Binary Tree from Preorder and Inorder Traversal
+# Given two integer arrays preorder and inorder
+# where preorder is the preorder traversal of a binary tree
+# and inorder is the inorder traversal of the same tree,
 # construct and return the binary tree.
 
-# !!! inorder and postorder consist of UNIQUE values !!!
+# !!! preorder and inorder consist of UNIQUE values !!!
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val, self.left, self.right = val, left, right
 
 class Solution:
-    def buildTree(self, inorder, postorder):
+    def buildTree(self, preorder, inorder):
         self.inorder = inorder
         self.inorder_map = {v: i for i, v in enumerate(inorder)}
-        self.postorder, self.post_ind = postorder, len(postorder)-1
+        self.preorder, self.pre_ind = preorder, 0
         return self.build_recurs(n1=0, n2=len(self.inorder)-1)
 
     def build_recurs(self, n1, n2):
         # n1 first, n2 last : inorder-indexes
         if n1 > n2: return None
-        tn = TreeNode(val=self.postorder[self.post_ind])
+        tn = TreeNode(val=self.preorder[self.pre_ind])
         inorder_ind = self.inorder_map[tn.val]
-        self.post_ind -= 1
-        # важен порядок вычисления: сначала tn.right, потом tn.left
-        tn.right = self.build_recurs(n1=inorder_ind + 1, n2=n2)
+        self.pre_ind += 1
+        # важен порядок вычисления: сначала tn.left, потом tn.right
         tn.left = self.build_recurs(n1=n1, n2=inorder_ind-1)
+        tn.right = self.build_recurs(n1=inorder_ind+1, n2=n2)
         return tn
 
 ########## TEST ########################################################################################################
 sln = Solution()
-root = sln.buildTree(inorder=[2,1,3], postorder=[2,3,1])
+root = sln.buildTree(preorder=[1,2,3], inorder=[2,1,3])
 print(root)

3c83ed05af0665fdb29c31b4956deb9cbbb91f39
Romzes <roman.usachev@gmail.com>
2023-03-10 18:52:48 +0300

    BinaryTree

--- MainProject/Explore/Learn/10_BinaryTree/2_Medium/00116.py
+++ MainProject/Explore/Learn/10_BinaryTree/2_Medium/00116.py
@@ -7,6 +7,7 @@ class TreeNode:
 # Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
 # Initially, all next pointers are set to NULL.
 
+# !!! Код решения задач 116 и 117 полностью совпадает !!!
 class Solution:
     def connect(self, root):
         if not root: return root
@@ -19,9 +20,8 @@ class Solution:
         next_level = []
         for i, tn in enumerate(curr_level):
             if i < len(curr_level) - 1: tn.next = curr_level[i+1]
-            if tn.left:
-                next_level.append(tn.left)
-                next_level.append(tn.right)
+            if tn.left: next_level.append(tn.left)
+            if tn.right: next_level.append(tn.right)
         return next_level
 
 ########## TEST ########################################################################################################
--- MainProject/Explore/Learn/10_BinaryTree/2_Medium/00117.py
+++ MainProject/Explore/Learn/10_BinaryTree/2_Medium/00117.py
@@ -6,9 +6,22 @@ class TreeNode:
 # Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
 # Initially, all next pointers are set to NULL.
 
+# !!! Код решения задач 116 и 117 полностью совпадает !!!
 class Solution:
     def connect(self, root):
-        pass
+        if not root: return root
+        level = [root]
+        while len(level) > 0:
+            level = self.calc_next_level(level)
+        return root
+
+    def calc_next_level(self, curr_level):
+        next_level = []
+        for i, tn in enumerate(curr_level):
+            if i < len(curr_level) - 1: tn.next = curr_level[i+1]
+            if tn.left: next_level.append(tn.left)
+            if tn.right: next_level.append(tn.right)
+        return next_level
 
 ########## TEST ########################################################################################################
 sln = Solution()

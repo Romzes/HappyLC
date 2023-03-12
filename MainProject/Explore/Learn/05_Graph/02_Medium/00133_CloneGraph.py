212f52dd98877605b162bc7a8b631c0552811400
Romzes <roman.usachev@gmail.com>
2023-03-12 23:57:16 +0300

    Graph

--- MainProject/Explore/Learn/05_Graph/02_Medium/00133_CloneGraph.py
+++ MainProject/Explore/Learn/05_Graph/02_Medium/00133_CloneGraph.py
@@ -1,6 +1,9 @@
 # Medium 133. Clone Graph
 # Given a reference of a node in a connected undirected graph. Return a deep copy (clone) of the graph.
 # Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
+# Node.val is UNIQUE for each node.
+# There are no repeated edges and no self-loops in the graph.
+# The Graph is connected and all nodes can be visited starting from the given node.
 
 class Node:
     def __init__(self, val=0, neighbors=None):
@@ -10,20 +13,19 @@ class Node:
 class Solution:
     def cloneGraph(self, node):
         if not node: return None
-        stack, visited, clones = [node], set(), {}
+        stack, clones = [node], {node: self.clone_node(node)}
         while len(stack) > 0:
             src = stack.pop()
-            if src in visited: continue
-            visited.add(src)
-            dst = self.get_cloned_node(src, clones)
-            for i, src_child in enumerate(src.neighbors):
-                dst.neighbors[i] = self.get_cloned_node(src_child, clones)
-                if src_child not in visited: stack.append(src_child)
+            cln = clones[src]
+            for i, src_neigh in enumerate(src.neighbors):
+                if src_neigh not in clones:
+                    clones[src_neigh] = self.clone_node(src_neigh)
+                    stack.append(src_neigh)
+                cln.neighbors[i] = clones[src_neigh]
         return clones[node]
 
-    def get_cloned_node(self, src, clones):
-        if src not in clones: clones[src] = Node(val=src.val, neighbors=len(src.neighbors) * [None])
-        return clones[src]
+    def clone_node(self, src):
+        return Node(val=src.val, neighbors=len(src.neighbors) * [None])
 
 ########## TEST ########################################################################################################
 nd1 = Node(1)

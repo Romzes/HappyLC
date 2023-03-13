839f1c7bc9f1cc33d903fbf63fab078635aa54a9
Romzes <roman.usachev@gmail.com>
2023-03-13 18:53:12 +0300

    Graph

--- MainProject/Explore/Learn/05_Graph/03_Hard/00332.py
+++ MainProject/Explore/Learn/05_Graph/03_Hard/00332.py
@@ -10,41 +10,41 @@ class Solution:
     def findItinerary(self, tickets):
         # tickets: List[List[str]], return List[str]
         self.create_ticket_graph(tickets)
-        self.min_route = self.min_route_join = None
+        self.min_route = None
         start_arp = 'JFK'
         self.curr_route = [start_arp]
-        for curr_arp in self.ticket_graph[start_arp]: self.proc_node(curr_arp)
+        for curr_arp in self.ticket_graph[start_arp]['children']: self.proc_node(curr_arp)
         return self.min_route
 
     def proc_node(self, curr_arp):
+        curr_ind = len(self.curr_route)
+        if self.min_route is not None and self.min_route[curr_ind] < curr_arp: return
+
         prev_arp = self.curr_route[-1]
-        self.ticket_graph[prev_arp][curr_arp] -= 1
         self.curr_route.append(curr_arp)
+        self.ticket_graph[prev_arp]['edges'][curr_arp] -= 1
 
         if len(tickets) == len(self.curr_route) - 1:
-            if self.min_route is None or len(self.curr_route) < len(self.min_route):
-                self.min_route = [arp for arp in self.curr_route]
-                self.min_route_join = '.'.join(self.min_route)
-            elif len(self.curr_route) == len(self.min_route):
-                curr_route_join = '.'.join(self.curr_route)
-                if curr_route_join < self.min_route_join:
-                    self.min_route = [arp for arp in self.curr_route]
-                    self.min_route_join = curr_route_join
+            self.min_route = [arp for arp in self.curr_route]
         elif curr_arp in self.ticket_graph:
-            for next_arp, cnt in self.ticket_graph[curr_arp].items():
+            for next_arp in self.ticket_graph[curr_arp]['children']:
+                cnt = self.ticket_graph[curr_arp]['edges'][next_arp]
                 if cnt == 0: continue
                 self.proc_node(next_arp)
 
         self.curr_route.pop()
-        self.ticket_graph[prev_arp][curr_arp] += 1
+        self.ticket_graph[prev_arp]['edges'][curr_arp] += 1
 
     def create_ticket_graph(self, tickets):
         self.ticket_graph = {}
         for t in tickets:
             src, dst = t[0], t[1]
-            if src not in self.ticket_graph: self.ticket_graph[src] = {}
-            if dst not in self.ticket_graph[src]: self.ticket_graph[src][dst] = 1
-            else: self.ticket_graph[src][dst] += 1
+            if src not in self.ticket_graph: self.ticket_graph[src] = dict(edges={})
+            edges = self.ticket_graph[src]['edges']
+            if dst not in edges: edges[dst] = 1
+            else: edges[dst] += 1
+        for item in self.ticket_graph.values():
+            item['children'] = sorted(item['edges'].keys())
 
 ########## TEST ########################################################################################################
 sln = Solution()

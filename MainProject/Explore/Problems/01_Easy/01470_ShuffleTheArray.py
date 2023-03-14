fbd91a1b92dc593e8f81ea1249827d074d8d7bce
Romzes <roman.usachev@gmail.com>
2023-03-15 00:04:08 +0300

    array

---
+++ MainProject/Explore/Problems/01_Easy/01470_ShuffleTheArray.py
@@ -0,0 +1,14 @@
+# Easy 1470. Shuffle the Array
+# Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+# Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+class Solution:
+    def shuffle(self, nums, n):
+        res = len(nums) * [None]
+        for i in range(len(nums)//2): res[2*i], res[2*i+1] = nums[i], nums[n+i]
+        return res
+
+########## TEST ########################################################################################################
+sln = Solution()
+nums = [1,2,3,4]
+print(sln.shuffle(nums, len(nums)//2))

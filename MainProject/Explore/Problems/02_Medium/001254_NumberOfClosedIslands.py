3b69d52b24406cf5e1b73d5da82b2ec74a410573
Romzes <roman.usachev@gmail.com>
2023-04-07 20:41:01 +0300

    medium

--- MainProject/Explore/Problems/02_Medium/001254_NumberOfClosedIslands.py
+++ MainProject/Explore/Problems/02_Medium/001254_NumberOfClosedIslands.py
@@ -6,25 +6,24 @@
 class Solution:
     def closedIsland(self, grid):
         self.grid, self.M, self.N = grid, len(grid), len(grid[0])  # M x N = rows x columns
+        self.m_edges, self.n_edges = (0, self.M-1), (0, self.N-1)
         self.LAND, self.WATER = 0, 1
         closed_cnt = 0
-        for i0 in range(self.M):
-            for j0 in range(self.N):
+        for i0 in range(1, self.M-1):
+            for j0 in range(1, self.N-1):
                 if self.grid[i0][j0] == self.LAND: closed_cnt += self.paint_island(i0, j0)
         return closed_cnt
 
     def paint_island(self, i0, j0):
-        is_closed, stack, ordered = 1, [(i0, j0)], set([(i0, j0)])
+        is_closed, stack = True, [(i0, j0)]
         while stack:
             i, j = stack.pop()
+            if self.grid[i][j] != self.LAND: continue
             self.grid[i][j] = 2
-            if i in (0, self.M-1) or j in (0, self.N-1): is_closed = 0
+            if is_closed and (i in self.m_edges or j in self.n_edges): is_closed = False
             for i1, j1 in [(i-1,j), (i+1,j), (i,j-1), (i,j+1)]:
-                k = (i1, j1)
-                if i1 < 0 or i1 >= self.M or j1 < 0 or j1 >= self.N or self.grid[i1][j1] != self.LAND or k in ordered:
-                    continue
-                stack.append(k)
-                ordered.add(k)
+                if i1 < 0 or i1 >= self.M or j1 < 0 or j1 >= self.N or self.grid[i1][j1] != self.LAND: continue
+                stack.append((i1, j1))
         return is_closed
 
 sln = Solution()

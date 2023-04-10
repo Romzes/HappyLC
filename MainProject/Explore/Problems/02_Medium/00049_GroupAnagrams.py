e6862c3ded2af8f94ae929a58e642006991f10ae
Romzes <roman.usachev@gmail.com>
2023-04-11 02:07:52 +0300

    str-array

---
+++ MainProject/Explore/Problems/02_Medium/00049_GroupAnagrams.py
@@ -0,0 +1,22 @@
+# Medium 49. Group Anagrams
+# Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+# Constraints: strs[i] consists of lowercase English letters.
+
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs):
+        d = defaultdict(list)
+        for s in strs: d['.'.join(sorted(s))].append(s)
+        return list(d.values())
+
+sln = Solution()
+print(sln.groupAnagrams(strs=['']))
+
+sln = Solution()
+print(sln.groupAnagrams(strs=['a']))
+
+sln = Solution()
+print(sln.groupAnagrams(strs=['eat','tea','tan','ate','nat','bat']))
+
+

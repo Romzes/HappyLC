eaa1c1e2e61a1e6b44dc6db9fcfe7536ad250b8e
Romzes <roman.usachev@gmail.com>
2023-04-22 05:32:05 +0300

    array

--- MainProject/Explore/Problems/03_Hard/00042_TrappingRainWater.py
+++ MainProject/Explore/Problems/03_Hard/00042_TrappingRainWater.py
@@ -2,17 +2,34 @@
 # Given n non-negative integers representing an elevation map where the width of each bar is 1,
 # compute how much water it can trap after raining.
 
+### O(n) space
 class Solution:
     def trap(self, height):
         if not height or len(height) <= 2: return 0
-        m1 = m2 = float('-inf'); water = len(height)*[0]; s = 0
-        for i in range(1, len(height)-1):
-            m1 = max(m1, height[i-1])
-            water[i] = m1
+        left_water = len(height)*[0]; right_max = s = 0
+        for i in range(1, len(height)-1): left_water[i] = max(left_water[i-1], height[i-1])
         for i in range(len(height)-2, 0, -1):
-            m2 = max(m2, height[i+1])
-            water[i] = max(0, min(m2, water[i]) - height[i])
-            s += water[i]
+            right_max = max(right_max, height[i+1])
+            s += max(0, min(left_water[i], right_max) - height[i])
+        return s
+
+### O(1) space, 2-pointers
+class Solution:
+    def trap(self, height):
+        if not height or len(height) <= 2: return 0
+        l = 1; r = len(height)-2
+        l_max = r_max = s = 0
+        while l <= r:
+            l_max = max(l_max, height[l-1])
+            r_max = max(r_max, height[r+1])
+            if l_max <= height[l]: l += 1
+            elif height[r] >= r_max: r -= 1
+            elif l_max <= r_max:
+                s += l_max - height[l]
+                l += 1
+            else:
+                s += r_max - height[r]
+                r -= 1
         return s
 
 sln = Solution()

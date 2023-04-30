2e953bb33b29d3942a60477123898e4003f1fdc3
Romzes <roman.usachev@gmail.com>
2023-04-30 03:03:03 +0300

    arr

--- MainProject/Explore/Problems/02_Medium/00022_GenerateParentheses.py
+++ MainProject/Explore/Problems/02_Medium/00022_GenerateParentheses.py
@@ -1,6 +1,7 @@
 # Medium 22. Generate Parentheses
 # Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
 
+### Recursive-1
 class Solution:
     def generateParenthesis(self, n):
         self.n = n; self.res = []; self.seq = []
@@ -19,6 +20,24 @@ class Solution:
             self.seq.append(')')
             self.recurs(open, close+1)
             self.seq.pop()
+
+### Recursive-2
+class Solution:
+    def generateParenthesis(self, n):
+        self.n = n; self.res = []; self.seq = 2*n*[None]
+        self.recurs(i=0, open=0, close=0)
+        return self.res
+
+    def recurs(self, i, open, close):
+        if i == 2*self.n:
+            self.res.append(''.join(self.seq))
+            return
+        if open < self.n:
+            self.seq[i] = '(';
+            self.recurs(i+1, open+1, close)
+        if open > close:
+            self.seq[i] = ')';
+            self.recurs(i+1, open, close+1)
 
 
 sln = Solution()

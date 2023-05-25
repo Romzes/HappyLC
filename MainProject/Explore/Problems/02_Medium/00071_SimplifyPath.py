485af8a57a32feb326a9eb74bcdc8527377e0ba6
Romzes <roman.usachev@gmail.com>
2023-05-26 00:05:31 +0300

    stack

--- MainProject/Explore/Problems/02_Medium/00071_SimplifyPath.py
+++ MainProject/Explore/Problems/02_Medium/00071_SimplifyPath.py
@@ -4,13 +4,15 @@
 
 class Solution:
     def simplifyPath(self, path):
-        stack, parts = [], [s for s in path.split(sep='/') if s not in ('', '.')]
+        stack = []; parts = [s for s in path.split(sep='/') if s not in ('', '.')]
         for s in parts:
             if s == '..':
                 if stack: stack.pop()
             else: stack.append(s)
         return '/' + '/'.join(stack)
 
+sln = Solution()
+print(sln.simplifyPath(path=r'/../'))
 
 sln = Solution()
 print(sln.simplifyPath(path=r'/.'))

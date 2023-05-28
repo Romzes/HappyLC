9301f85fa75508ba5f68f60745d2212e60ed40b4
Romzes <roman.usachev@gmail.com>
2023-05-29 02:03:03 +0300

    sort

--- MainProject/Explore/Problems/02_Medium/00253_MeetingRooms_II.py
+++ MainProject/Explore/Problems/02_Medium/00253_MeetingRooms_II.py
@@ -4,12 +4,13 @@
 
 class Solution:
     def minMeetingRooms(self, intervals):
-        moments, j = 2*len(intervals)*[None], 0
+        # intervals: List[List[int]]
+        moments = 2*len(intervals)*[None]; j = 0
         for itv in intervals:
             moments[j], moments[j+1] = (itv[0], 1), (itv[1], -1)
             j += 2
         moments.sort(key=lambda mnt: mnt[0] + 0.1 * mnt[1])
-        sum, rooms = 0, -1e10
+        sum = 0; rooms = -1
         for mnt in moments:
             sum += mnt[1]
             if sum > rooms: rooms = sum

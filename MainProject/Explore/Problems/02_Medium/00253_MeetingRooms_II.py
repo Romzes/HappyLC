272af4fedd5b2c3d40aff7258c7b22077b83361c
Romzes <roman.usachev@gmail.com>
2024-01-18 00:58:33 +0300

    Yandex

--- MainProject/Explore/Problems/02_Medium/00253_MeetingRooms_II.py
+++ MainProject/Explore/Problems/02_Medium/00253_MeetingRooms_II.py
@@ -2,8 +2,10 @@
 # Given an array of meeting time intervals intervals where intervals[i] = [ start[i], end[i] )
 # return the minimum number of conference rooms required.
 
+from typing import List
+
 class Solution:
-    def minMeetingRooms(self, intervals):
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         # intervals: List[List[int]]
         moments = 2*len(intervals)*[None]; j = 0
         for itv in intervals:
@@ -17,7 +19,7 @@ class Solution:
         return rooms
 
 class Solution:
-    def minMeetingRooms(self, intervals):
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         n = len(intervals); beg = n*[None]; end = n*[None]
         for i, itv in enumerate(intervals): beg[i] = itv[0]; end[i] = itv[1]
         beg.sort(); end.sort()
@@ -27,6 +29,19 @@ class Solution:
             elif end[j] < beg[i]: sum -= 1; j += 1
             else: i += 1; j += 1
             rooms = max(rooms, sum)
+        return rooms
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        arr = 2 * len(intervals) * [None]; i = 0
+        for j, itv in enumerate(intervals):
+            arr[i], arr[i+1] = (itv[0], 1), (itv[1], -1)
+            i += 2
+        arr.sort()
+        s = rooms = 0
+        for a in arr:
+            s += a[1]
+            rooms = max(rooms, s)
         return rooms
 
 sln = Solution()

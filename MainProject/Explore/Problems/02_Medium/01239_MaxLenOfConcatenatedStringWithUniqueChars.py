699019ff104e9e27c7a321cfe2af594b779113a5
Romzes <roman.usachev@gmail.com>
2024-01-23 15:04:49 +0300

    arr

--- MainProject/Explore/Problems/02_Medium/01239_MaxLenOfConcatenatedStringWithUniqueChars.py
+++ MainProject/Explore/Problems/02_Medium/01239_MaxLenOfConcatenatedStringWithUniqueChars.py
@@ -9,8 +9,41 @@ Constraints:
   1 <= arr[i].length <= 26
   arr[i] contains only lowercase English letters.
 """
+import string
 from typing import List
+
 class Solution:
+    # Dinar
+    def maxLength(self, arr: List[str]) -> int:
+        nums = set()
+        def convert(s):
+            n = 0
+            for char in s:
+                idx = 1 << (ord(char) - ord('a'))
+                if n & idx:
+                    return -1
+                n |= idx
+            return n
+        for s in arr:
+            n = convert(s)
+            if n != -1 and n not in nums:
+                nums.add(n)
+        ans = 0
+        dp = []
+        for n1 in nums:
+            for i in range(len(dp)):
+                n2 = dp[i]
+                if n1 & n2 == 0:
+                    new_n = n1 | n2
+                    dp.append(new_n)
+                    ans = max(ans, new_n.bit_count())
+            dp.append(n1)
+            ans = max(ans, n1.bit_count())
+        print(f'dp = {len(dp)}')
+        return ans
+
+class Solution:
+    # Roman
     def maxLength(self, arr: List[str]) -> int:
         self.nums = [0]
         for s in arr:
@@ -39,6 +72,9 @@ class Solution:
             if v & b != 0: return -1
             else: v |= b
         return v
+
+sln = Solution()
+print(sln.maxLength(arr=list(string.ascii_lowercase)))
 
 sln = Solution()
 print(sln.maxLength(arr=['un', 'iq', 'ue']))

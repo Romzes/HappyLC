35d3ffe8f764ed98eeb17eaf2b521562409bd2b2
Romzes <roman.usachev@gmail.com>
2024-01-26 05:38:10 +0300

    dp

---
+++ MainProject/Explore/Problems/02_Medium/00576_OutOfBoundaryPaths.py
@@ -0,0 +1,51 @@
+"""
+576 (Medium) Out of Boundary Paths
+There is an m x n grid with a ball. The ball is initially at the position [startRow, startColumn].
+You are allowed to move the ball to one of the four adjacent cells in the grid (possibly out of the grid crossing the grid boundary).
+You can apply at most maxMove moves to the ball.
+Given the five integers m, n, maxMove, startRow, startColumn,
+return the number of paths to move the ball out of the grid boundary.
+Since the answer can be very large, return it modulo 10^9 + 7.
+Constraints:
+  1 <= m, n <= 50
+  0 <= maxMove <= 50
+  0 <= startRow < m
+  0 <= startColumn < n
+"""
+
+class Solution:
+    def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
+        if maxMove == 0: return 0
+        if maxMove == 1: return (startRow == 0) + (startRow == m-1) + (startColumn == 0) + (startColumn == n-1)
+        b1 = self.create_board(m, n); b2 = self.create_board(m, n)
+        for k in range(maxMove-1):
+            for i in range(1, m+1):
+                for j in range(1, n+1):
+                    b2[i][j] = b1[i-1][j] + b1[i+1][j] + b1[i][j-1] + b1[i][j+1]
+            b1, b2 = b2, b1
+        i, j = startRow+1, startColumn+1
+        res = b1[i-1][j] + b1[i+1][j] + b1[i][j-1] + b1[i][j+1]
+        return res % (10**9 + 7)
+
+    def create_board(self, m, n):
+        b = [(n+2)*[0] for _ in range(m+2)]
+        for j in range(n+2): b[0][j] = b[-1][j] = 1  # 2 крайних строки
+        for i in range(m+2): b[i][0] = b[i][-1] = 1  # 2 крайних столбца
+        return b
+
+from functools import cache
+class Solution:
+    def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
+        @cache
+        def rec(i, j, maxMove):
+            if i == -1 or i == m or j == -1 or j == n: return 1
+            if maxMove == 0: return 0
+            maxMove -= 1
+            return rec(i-1, j, maxMove) + rec(i+1, j, maxMove) + rec(i, j-1, maxMove) + rec(i, j+1, maxMove)
+        return rec(startRow, startColumn, maxMove) % (10**9 + 7)
+
+sln = Solution()
+print(sln.findPaths(m=2, n=2, maxMove=2, startRow=0, startColumn=0))
+
+sln = Solution()
+print(sln.findPaths(m=1, n=3, maxMove=3, startRow=0, startColumn=1))

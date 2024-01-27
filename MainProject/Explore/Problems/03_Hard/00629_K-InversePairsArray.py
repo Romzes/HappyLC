d3aa92fd8f519763877a3bf898a237359d632d0e
Romzes <roman.usachev@gmail.com>
2024-01-27 22:01:56 +0300

    dp

--- MainProject/Explore/Problems/03_Hard/00629_K-InversePairsArray.py
+++ MainProject/Explore/Problems/03_Hard/00629_K-InversePairsArray.py
@@ -31,6 +31,34 @@ class Solution:
             return rec(n-1, k) + rec(n, k-1) - rec(n-1, k-n)
         return rec(n, k) % (10**9 + 7)
 
+class Solution:
+    def kInversePairs(self, n: int, k: int) -> int:
+        if k == 0: return 1
+        if n == 1: return int(k == 0)
+        # n > 1, k > 0
+        dp = [(k+1)*[0] for _ in range(n+1)]
+        for n1 in range(1, n+1): dp[n1][0] = 1
+        for n1 in range(2, n+1):
+            for k1 in range(1, k+1):
+                d = k1 - n1
+                dp[n1][k1] = dp[n1-1][k1] + dp[n1][k1-1] - (dp[n1-1][d] if d >= 0 else 0)
+        return dp[n][k] % (10**9 + 7)
+
+class Solution:
+    def kInversePairs(self, n: int, k: int) -> int:
+        if k == 0: return 1
+        if n == 1: return int(k == 0)
+        # n > 1, k > 0
+        mod = 10**9 + 7; r1 = (k+1)*[0]; r1[0] = 1; r2 = (k+1)*[0]
+        for n1 in range(2, n+1):
+            r2[0] = 1
+            for k1 in range(1, k+1):
+                d = k1 - n1
+                r2[k1] = r1[k1] + r2[k1-1] - (r1[d] if d >= 0 else 0)
+                r2[k1] %= mod
+            r1, r2 = r2, r1
+        return r1[-1]
+
 sln = Solution()
 print(sln.kInversePairs(n=3, k=0))
 

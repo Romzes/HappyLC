b9fad4f0a25e6a596a8775888b53035c6ec9bf44
Romzes <roman.usachev@gmail.com>
2024-02-09 04:36:56 +0300

    largestDivisibleSubset

--- MainProject/Explore/Problems/02_Medium/00368_LargestDivisibleSubset.py
+++ MainProject/Explore/Problems/02_Medium/00368_LargestDivisibleSubset.py
@@ -11,6 +11,9 @@ Constraints:
   All the integers in nums are unique.
 """
 from typing import List
+
+# Runtime 209 ms , Beats 84.47% of users with Python3
+# Memory 16.95 MB , Beats 65.23%of users with Python3
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
         n = len(nums)
@@ -34,6 +37,40 @@ class Solution:
         res.reverse()
         return res
 
+# Runtime 105 ms , Beats 100.00% of users with Python3
+# Memory 16.96 MB , Beats 65.23% of users with Python3
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        if n == 1: return [nums[0]]
+        nums.sort()
+        dp = {nums[0]: [1, None]}  # [max_len, prev_val]
+        max_len = 1; max_val = nums[0]
+        for i in range(1, n):
+            v = nums[i]
+            dp[v] = [1, None]
+            sqrt_v = int(v**0.5)
+            if i <= 2*sqrt_v:
+                for j in range(i-1, -1, -1):
+                    u = nums[j]
+                    if v % u == 0 and dp[v][0] <= dp[u][0]:
+                        dp[v][0], dp[v][1] = dp[u][0] + 1, u
+            else:
+                for j in range(1, sqrt_v+1):
+                    w, r = divmod(v, j)
+                    if r != 0: continue
+                    for u in (w, j):
+                        if u in dp and dp[v][0] <= dp[u][0]:
+                            dp[v][0], dp[v][1] = dp[u][0] + 1, u
+            if dp[v][0] > max_len: max_len, max_val = dp[v][0], v
+        res = []
+        v = max_val
+        while v is not None:
+            res.append(v)
+            v = dp[v][1]
+        res.reverse()
+        return res
+
 sln = Solution()
 print(sln.largestDivisibleSubset(nums=[3,4,16,8]))
 
@@ -44,6 +81,3 @@ sln = Solution()
 print(sln.largestDivisibleSubset(nums=[1,2,4,8]))
 
 
-# sqrt_v = int(v**0.5)
-# if i <= 2*sqrt_v:
-#     for j in range(i): dp[v] = max(dp[v], 1+dp[nums[j]])

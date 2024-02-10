e2f8da0ff73db865a7ce6149928f4bf9b2425ecd
Romzes <roman.usachev@gmail.com>
2024-02-11 02:32:08 +0300

    longestSubarray

--- MainProject/Explore/Problems/02_Medium/01493_LongestSubarrayOf_1_AfterDeletingOneElement.py
+++ MainProject/Explore/Problems/02_Medium/01493_LongestSubarrayOf_1_AfterDeletingOneElement.py
@@ -9,35 +9,49 @@ Constraints:
 """
 from typing import List
 
+# Runtime 423 ms , Beats 93.00% of users with Python3
+# Memory 19.49 MB , Beats 94.22% of users with Python3
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        i0 = None  # индекс последнего 0
-        max_sum1 = prev1 = sum1 = 0  # ...0111011... => prev1 = 3, sum1 = 5
+        j0 = -1  # индекс последнего 0
+        max_sum1 = sum1 = 0  # ...011101(1)... => sum1 = 5
         for i, v in enumerate(nums):
             if v == 1:
                 sum1 += 1
             else:
                 max_sum1 = max(max_sum1, sum1)
-                i0 = i
-                prev1 = sum1 = sum1 - prev1
-        max_sum1 = max(max_sum1, sum1)
-        if i0 is None: max_sum1 -= 1  # нулей нет в массиве nums => нужно обязательно удалить одну единицу
+                sum1 = i - j0 - 1
+                j0 = i
+        return min(max(max_sum1, sum1), len(nums)-1)
+
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        j0 = -1  # индекс последнего 0
+        max_sum1 = sum1 = 0  # ...011101(1)... => sum1 = 5
+        for i, v in enumerate(nums):
+            if v == 1:
+                sum1 += 1
+                max_sum1 = max(max_sum1, sum1)
+            else:
+                sum1 = i - j0 - 1
+                j0 = i
+        if j0 == -1: max_sum1 -= 1  # нулей нет в массиве nums => нужно обязательно удалить одну единицу
         return max_sum1
 
+
 ### ЭТАЛОННОЕ РЕШЕНИЕ ДЛЯ СОБЕСА
+# Runtime 402 ms , Beats 99.67% of users with Python3
+# Memory 20.04 MB , Beats 66.82% of users with Python3
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        i0 = None  # индекс последнего 0
         max_sum1 = prev1 = sum1 = 0  # ...0111011... => prev1 = 3, sum1 = 5
-        for i, v in enumerate(nums):
+        for v in nums:
             if v == 1:
                 sum1 += 1
-                max_sum1 = max(max_sum1, sum1)
             else:
-                i0 = i
+                if sum1 > max_sum1: max_sum1 = sum1
                 prev1 = sum1 = sum1 - prev1
-        if i0 is None: max_sum1 -= 1  # нулей нет в массиве nums => нужно обязательно удалить одну единицу
-        return max_sum1
+        return min(max(max_sum1, sum1), len(nums) - 1)
 
 sln = Solution()
 print(sln.longestSubarray(nums=[0,0,0]))

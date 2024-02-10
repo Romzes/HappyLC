8b39ed12b8af6724e02e91393bceaede97321ba0
Romzes <roman.usachev@gmail.com>
2024-02-10 05:07:39 +0300

    Yandex

--- MainProject/Companies/Yandex/CountCommonElementsForPrefixes.py
+++ MainProject/Companies/Yandex/CountCommonElementsForPrefixes.py
@@ -21,15 +21,17 @@ Note: solve with O(N) time and space complexity.
 
 def calc_nums(arr1, arr2):
     n = len(arr1)
-    res_arr = n * [0]; res_set = set()
+    res_arr = n * [0]
     stat1, stat2 = {}, {}
     for i in range(n):
         v1, v2 = arr1[i], arr2[i]
         if v1 not in stat1: stat1[v1] = i
         if v2 not in stat2: stat2[v2] = i
-        if stat1.get(v2, n) <= i: res_set.add(v2)
-        if stat2.get(v1, n) <= i: res_set.add(v1)
-        res_arr[i] = len(res_set)
+        cnt = 0
+        if max(stat1[v1], stat2.get(v1, n)) == i: cnt += 1
+        if v2 != v1 and max(stat2[v2], stat1.get(v2, n)) == i: cnt += 1
+        if i > 0: res_arr[i] += res_arr[i - 1]
+        res_arr[i] += cnt
     return res_arr
 
 arr1 = [1,1,2,3]

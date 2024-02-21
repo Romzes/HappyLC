920b18eec2c0b6a2d3a545acdcbbc4de790ee0ce
Romzes <roman.usachev@gmail.com>
2024-02-21 16:41:16 +0300

    rangeBitwiseAnd

---
+++ MainProject/Explore/Problems/02_Medium/00201_BitwiseANDofNumbersRange.py
@@ -0,0 +1,27 @@
+"""
+201 (Medium) Bitwise AND of Numbers Range
+Given two integers left and right that represent the range [left, right],
+return the bitwise AND of all numbers in this range, inclusive.
+Constraints:
+  0 <= left <= right <= 2^31 - 1
+"""
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        if left == 0 or left == right: return left
+        ls, rs = '{0:b}'.format(left), '{0:b}'.format(right)
+        # print(ls, rs)
+        if len(ls) < len(rs): return 0
+        res = len(ls) * ['0']
+        for i in range(len(ls)):
+            if ls[i] < rs[i]: break
+            if ls[i] == rs[i] and rs[i] == '1': res[i] = '1'
+        return int(''.join(res), 2)
+
+sln = Solution()
+print(sln.rangeBitwiseAnd(left=5, right=7))
+
+sln = Solution()
+print(sln.rangeBitwiseAnd(left=0, right=0))
+
+sln = Solution()
+print(sln.rangeBitwiseAnd(left=1, right=2147483647))

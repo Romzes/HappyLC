fe29be2d1dcab5e517d776fed30b170d061126dd
Romzes <roman.usachev@gmail.com>
2024-03-03 02:42:13 +0300

    sortedSquares

--- MainProject/Explore/Problems/01_Easy/00977_SquaresOfSortedArray.py
+++ MainProject/Explore/Problems/01_Easy/00977_SquaresOfSortedArray.py
@@ -6,16 +6,14 @@ return an array of the squares of each number sorted in non-decreasing order.
 from typing import List
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        lng = len(nums)
-        res = lng * [0]
-        for i in range(lng): nums[i] **= 2
-        i = 0; j = k = lng-1
-        while k >= 0:
+        lng = len(nums); res = lng * [0]
+        for i in range(lng): nums[i] = nums[i] * nums[i]
+        i, j = 0, lng-1
+        for k in range(lng-1, -1, -1):
             if nums[i] >= nums[j]:
                 res[k] = nums[i]; i += 1
             else:
                 res[k] = nums[j]; j -= 1
-            k -= 1
         return res
 
 sln = Solution()

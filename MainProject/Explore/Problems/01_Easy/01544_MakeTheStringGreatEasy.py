4dfcdf68faa2339dda4ee5456ed47dfa42092a3e
Romzes <roman.usachev@gmail.com>
2024-04-05 19:12:13 +0300

    makeGood

--- MainProject/Explore/Problems/01_Easy/01544_MakeTheStringGreatEasy.py
+++ MainProject/Explore/Problems/01_Easy/01544_MakeTheStringGreatEasy.py
@@ -1,6 +1,20 @@
 """
 1544 (Easy) Make The String Great
 """
+import string
+
+class Solution:
+    def makeGood(self, s: str) -> str:
+        stack = []
+        for c in s:
+            if stack and abs(ord(c) - ord(stack[-1])) == 32:
+                stack.pop()
+            else:
+                stack.append(c)
+        return ''.join(stack)
+
+# Runtime=33ms Beats 83.07% of users with Python3
+# Memory=16.81MB Beats 11.30% of users with Python3
 class Solution:
     def makeGood(self, s: str) -> str:
         stack = []
@@ -11,7 +25,6 @@ class Solution:
                 stack.append(c)
         return ''.join(stack)
 
-
 sln = Solution()
 print(sln.makeGood(s='leEeetcode'))
 
@@ -20,3 +33,5 @@ print(sln.makeGood(s='abBAcC'))
 
 sln = Solution()
 print(sln.makeGood(s='s'))
+
+# for i, c in enumerate(string.ascii_lowercase): print(ord(string.ascii_uppercase[i]) - ord(c))

c045ec29361b0bb17e43ac44ef4547d6f99b3925
Romzes <roman.usachev@gmail.com>
2025-06-14 17:31:05 +0300

    1493

--- MainProject/Explore/Problems/02_Medium/01493_LongestSubarrayOf_1_AfterDeletingOneElement.py
+++ MainProject/Explore/Problems/02_Medium/01493_LongestSubarrayOf_1_AfterDeletingOneElement.py
@@ -3,6 +3,8 @@
 Given a binary array nums, you should delete one element from it.
 Return the size of the longest non-empty subarray containing only 1's in the resulting array.
 Return 0 if there is no such subarray.
+!!! НУЖНО ОБЯЗАТЕЛЬНО УДАЛИТЬ 1 ЭЛЕМЕНТ !!!
+
 Constraints:
   1 <= nums.length <= 10^5
   nums[i] is either 0 or 1.
@@ -39,9 +41,7 @@ class Solution:
         return max_sum1
 
 
-### ЭТАЛОННОЕ РЕШЕНИЕ ДЛЯ СОБЕСА
-# Runtime 402 ms , Beats 99.67% of users with Python3
-# Memory 20.04 MB , Beats 66.82% of users with Python3
+# Runtime 402 ms , Beats 99.67%  ;  Memory 20.04 MB , Beats 66.82%
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
         max_sum1 = prev1 = sum1 = 0  # ...0111011... => prev1 = 3, sum1 = 5
@@ -53,17 +53,40 @@ class Solution:
                 prev1 = sum1 = sum1 - prev1
         return min(max(max_sum1, sum1), len(nums) - 1)
 
-sln = Solution()
-print(sln.longestSubarray(nums=[0,0,0]))
+
+### ЭТАЛОННОЕ РЕШЕНИЕ ДЛЯ СОБЕСА
+# Runtime 43 ms , Beats 81.15%  ;  Memory 21.92 MB , Beats 7.66%
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        has_zero = False
+        res = 0
+        cnt_tot = cnt = 0  # cnt_tot >= cnt
+        for a in nums:
+            # пример: nums = .. 0 1 1 1 0 [cnt = 1 1] a .. ; cnt_tot = 5, cnt = 2
+            if a == 1:
+                cnt += 1
+                cnt_tot += 1
+            else:
+                # nums = .. 0 1 1 1 0 1 1 0 .. ; cnt_tot = 3, cnt = 0
+                has_zero = True
+                cnt_tot = cnt
+                cnt = 0
+            res = max(res, cnt_tot)
+        if not has_zero: res = len(nums)-1
+        return res
+
 
 sln = Solution()
-print(sln.longestSubarray(nums=[1,1,1]))
+print(sln.longestSubarray(nums=[1,1,0,1]))  # Output: 3
 
 sln = Solution()
-print(sln.longestSubarray(nums=[1,1,0,1]))
+print(sln.longestSubarray(nums=[0,1,1,1,0,1,1,0,1]))  # Output: 5
 
 sln = Solution()
-print(sln.longestSubarray(nums=[0,1,1,1,0,1,1,0,1]))
+print(sln.longestSubarray(nums=[1,1,1]))  # Output: 2
 
 sln = Solution()
-print(sln.longestSubarray(nums=[1,1,1,0,1,1,0]))
+print(sln.longestSubarray(nums=[0,0,0]))  # Output: 0
+
+sln = Solution()
+print(sln.longestSubarray(nums=[1,1,1,0,1,1,0]))  # Output: 5

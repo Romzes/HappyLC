d7626d1f8066127584bb5a08cba8e4d10a32aec3
Romzes <roman.usachev@gmail.com>
2025-06-19 02:31:40 +0300

    array

--- MainProject/Explore/Problems/02_Medium/00054_SpiralMatrix.py
+++ MainProject/Explore/Problems/02_Medium/00054_SpiralMatrix.py
@@ -16,13 +16,34 @@ class Solution:
                 i, j = i + vectors[vi][0], j + vectors[vi][1]
         return res
 
+class Solution:
+    def spiralOrder(self, matrix):
+        m = len(matrix); n = len(matrix[0]); cnt = m*n; res = cnt * [None]
+        r1, r2, c1, c2 = 0, m-1, 0, n-1
+        r, c = 0, 0
+        dirs = ((0,1), (1,0), (0,-1), (-1,0))  # (delta_col, delta_row)
+        i = 0
+        deltas = ((1,0,0,0), (0,0,0,-1), (0,-1,0,0), (0,0,1,0))
+        for k in range(cnt):
+            res[k] = matrix[r][c]
+            dir = dirs[i]
+            if not (r1 <= r + dir[0] <= r2 and c1 <= c + dir[1] <= c2):
+                dlt = deltas[i]
+                r1 += dlt[0]; r2 += dlt[1]; c1 += dlt[2]; c2 += dlt[3]
+                i = (i + 1) % 4
+                dir = dirs[i]
+            r += dir[0]
+            c += dir[1]
+        return res
+
+
 sln = Solution()
 matrix = [
     [1,2,3],
     [4,5,6],
     [7,8,9]
 ]
-print(sln.spiralOrder(matrix))
+print(sln.spiralOrder(matrix))  # Output: [1,2,3,6,9,8,7,4,5]
 
 sln = Solution()
 matrix = [
@@ -30,4 +51,4 @@ matrix = [
     [5, 6, 7, 8],
     [9,10,11,12]
 ]
-print(sln.spiralOrder(matrix))
+print(sln.spiralOrder(matrix))  # Output: [1,2,3,4,8,12,11,10,9,5,6,7]

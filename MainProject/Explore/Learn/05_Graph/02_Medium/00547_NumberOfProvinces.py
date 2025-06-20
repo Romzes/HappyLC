c1fc6264bbeb7ab9221a654165aaba4cd00d7fdf
Romzes <roman.usachev@gmail.com>
2025-06-20 16:34:42 +0300

    dp

--- MainProject/Explore/Learn/05_Graph/02_Medium/00547_NumberOfProvinces.py
+++ MainProject/Explore/Learn/05_Graph/02_Medium/00547_NumberOfProvinces.py
@@ -2,12 +2,14 @@
 # There are n cities. Some of them are connected, while some are not.
 # If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
 # A province is a group of directly or indirectly connected cities and no other cities outside of the group.
-# You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly connected, and isConnected[i][j] = 0 otherwise.
+# You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the i-th city and the j-th city are directly connected, and isConnected[i][j] = 0 otherwise.
 # Return the total number of provinces.
 
+# Runtime = 3 ms  Beats 89.39%  ;  Memory = 19.06 MB  Beats 57.19%
 class Solution:
     def findCircleNum(self, isConnected):
-        self.isConnected, self.cities = isConnected, len(isConnected)*[-1]
+        self.isConnected = isConnected
+        self.cities = len(isConnected) * [-1]  # необработанные города помечены = -1
         prov = 0
         for c0 in range(len(isConnected)):
             if self.cities[c0] != -1: continue
@@ -16,10 +18,12 @@ class Solution:
         return prov
 
     def find_province(self, c0, prov):
+        # все города, лежащие в одной провинции вместе с городом-c0, получат номер провинции = prov
+        # self.cities[c0-провинция] = prov
         stack = [c0]
         while stack:
             c1 = stack.pop()
-            if self.cities[c1] != -1: continue
+            if self.cities[c1] != -1: continue  # город-c1 уже принадлежит некоторой провинции prov
             self.cities[c1] = prov
             for c2, flag in enumerate(self.isConnected[c1]):
                 if flag == 0 or self.cities[c2] != -1: continue
@@ -28,8 +32,8 @@ class Solution:
 
 sln = Solution()
 isConnected=[[1,1,0],[1,1,0],[0,0,1]]
-print(sln.findCircleNum(isConnected))
+print(sln.findCircleNum(isConnected))  # Output: 2
 
 sln = Solution()
 isConnected = [[1,0,0],[0,1,0],[0,0,1]]
-print(sln.findCircleNum(isConnected))
+print(sln.findCircleNum(isConnected))  # Output: 3

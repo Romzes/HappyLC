73b8c6b941edb535c414e7ba3496b9fcd3c981ff
Romzes <roman.usachev@gmail.com>
2025-07-06 19:18:16 +0300

    79_WordSearch

--- MainProject/Explore/Learn/80_Backtracking/00079_WordSearch.py
+++ MainProject/Explore/Learn/80_Backtracking/00079_WordSearch.py
@@ -14,6 +14,7 @@ Constraints:
 
 from typing import List
 
+# простая рекурсия без оптимизации, board не меняется - используется self.cell_path = set
 # Runtime = 5610ms  Beats 17.60%  ;  Memory = 17.86MB  Beats 64.69%
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
@@ -37,6 +38,28 @@ class Solution:
         for r2, c2 in ((r+1, c), (r-1, c), (r, c+1), (r, c-1)):
             if self.find_word(r=r2, c=c2, w_ind=w_ind+1): return True
         self.cell_path.remove(cell)
+        return False
+
+# рекурсия: board не меняется, ограничивается число рекурсивных вызовов функции find_word
+# Runtime = 3592ms  Beats 68.23%  ;  Memory = 17.66MB  Beats 95.98%
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        self.word = word
+        self.board, self.M, self.N = board, len(board), len(board[0])
+        for r in range(self.M):
+            for c in range(self.N):
+                if board[r][c] != word[0]: continue
+                if self.find_word(r, c, w_ind=0): return True
+        return False
+
+    def find_word(self, r, c, w_ind):
+        # инвариант board[r][c] = word[w_ind]
+        if w_ind == len(self.word) - 1: return True
+        self.board[r][c] = '*'
+        for r2, c2 in ((r+1, c), (r-1, c), (r, c+1), (r, c-1)):
+            if r2 < 0 or self.M <= r2 or c2 < 0 or self.N <= c2 or self.board[r2][c2] != self.word[w_ind+1]: continue
+            if self.find_word(r=r2, c=c2, w_ind=w_ind+1): return True
+        self.board[r][c] = self.word[w_ind]
         return False
 
 
